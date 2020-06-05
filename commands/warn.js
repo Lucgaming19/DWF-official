775956d15b8417d5339215b0420bc918b7642d14
Lucgaming19 <61281925+Lucgaming19@users.noreply.github.com>
2020-06-05 08:16:03 +0200

    init
    init

---
+++ commands/warn.js
@@ -0,0 +1,54 @@
+const discord = require("discord.js")
+const fs = require("fs");
+const warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
+
+module.exports.run = async (bot, message, args) => {
+
+	if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("This command is management only");
+
+	if (!args[0]) return message.reply("no username found!");
+
+	if (!args[1]) return message.reply("no reason found!");
+
+	if (!message.guild.me.hasPermission("KICK_MEMBERS")) return message.reply("No perms");
+
+	var warnUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(arg[0]));
+
+	var reason = args.slice(1).join(" ");
+
+	if (!warnUser) return message.reply("User not found!");
+
+	if (warnUser.hasPermission("KICK_MEMBERS")) return message.reply("sorry you can't warn him. he is a higher or equal to you.");
+
+	if (!warns[warnUser.id]) warns[warnUser.id] = {
+		warns: 0
+	};
+
+	warns[warnUser.id].warns++;
+
+	fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
+		if (err) console.log(err);
+	});
+
+	var embed = new discord.MessageEmbed()
+		.setColor("#ff0000")
+		.setFooter(message.member.displayName, message.author.displayAvatarURL)
+		.setTimestamp()
+		.setDescription(`**Gewarnd:** ${warnUser} (${warnUser.id})
+        **Warning by:** ${message.author}
+        **Reason: ** ${reason}`)
+		.addField("Warn count:", warns[warnUser.id].warns);
+
+	var channel = message.member.guild.channels.cache.get("716343529449717821");
+
+	if (!channel) return;
+
+	channel.send(embed);
+	warnUser.send(embed);
+
+}
+
+module.exports.help = {
+	name: "warn"
+
+}
