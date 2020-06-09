5e34c62707e39b79a0c0ee3eb11fe2303192823a
Lucgaming19 <61281925+Lucgaming19@users.noreply.github.com>
2020-06-09 13:20:16 +0200

    Update warnlevel.js

--- commands/warnlevel.js
+++ commands/warnlevel.js
@@ -4,12 +4,24 @@ const warns = JSON.parse(fs.readFileSync("./warns.json", "utf8"));
 
 module.exports.run = async (bot, message, args) => {
 
-    if (!message.member.hasPermission("MANAGE_Messages")) return message.reply("You can't do that IDIOTA!");
-    var warnUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(arg[0]));
-    if (!warnUser) return message.reply("User not found!");
+	if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("This command is management only");
+
+	if (!args[0]) return message.reply("no username found!");
+
+	if (!args[1]) return message.reply("no reason found!");
+
+	if (!message.guild.me.hasPermission("KICK_MEMBERS")) return message.reply("No perms");
+
+	var warnUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(arg[0]));
+
+	var reason = args.slice(1).join(" ");
+
+	if (!warnUser) return message.reply("User not found!");
+
+	if (warnUser.hasPermission("KICK_MEMBERS")) return message.reply("sorry you can't warn him. he is a higher or equal to you.");
     var warnlevel = warns[warnUser.id].warns;
 
-    message.reply(`<@${warnUser.id}> has ${warnlevel} warnings.`);
+    message.reply(`${warnUser} has ${warnlevel} warnings.`);
 
 
 }
