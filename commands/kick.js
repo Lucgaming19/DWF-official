7a6e88cb07f2e110d697fb1752e1083a9128b384
Lucgaming19 <61281925+Lucgaming19@users.noreply.github.com>
2020-06-21 16:54:12 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -45,7 +45,7 @@ module.exports.run = async (bot, message, args) => {
 			});
 
 			message.channel.send(embed);
-			message.kickUser.send(embed);
+			kickUser.send(embed);
 		} else if (emoji === "❌") {
 
 			msg.delete();
